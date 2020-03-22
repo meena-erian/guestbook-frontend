20f9bd4a936f63ffed5b85f8cfdd696c9ff95d92
Meena Erian <meena5.erian@gmail.com>
2020-03-22 17:52:07 +0200

    add client-side form validation for login and signup

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,13 +1,31 @@
 import React from "react";
 
 class LoginPage extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            validUsername : true,
+            validPassword : true,
+            passwordsMatch : true
+        }
+        this.handlePasswordInput = this.handlePasswordInput.bind(this);
+        this.handleUsernameInput = this.handleUsernameInput.bind(this);
+    }
+    handleUsernameInput(e){
+        let usernameRegex = /^[a-zA-Z0-9]{5,30}$/;
+        this.setState({validUsername : usernameRegex.test(e.target.value)})
+    }
+    handlePasswordInput(e){
+        let passwordRegex = /^.{7,200}$/;
+        this.setState({validPassword : passwordRegex.test(e.target.value)})
+    }
   render() {
     return (
       <div>
         <div className="AuthenticationForm">
           <h4>Login to continue</h4>
-          <input type="text" placeholder="Username" formNoValidate />
-          <input type="password" placeholder="Password" formNoValidate />
+          <input id="loginName" className={this.state.validUsername? "" : "badInput"} onChange = {this.handleUsernameInput} type="text" placeholder="Username" formNoValidate />
+          <input id="loginPassword" className={this.state.validPassword? "" : "badInput"} onChange = {this.handlePasswordInput} type="password" placeholder="Password" formNoValidate />
           <input type="button" value="Login" />
         </div>
       </div>
--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -1,14 +1,35 @@
 import React from "react";
 
 class SignupPage extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            validUsername : true,
+            validPassword : true,
+            passwordsMatch : true
+        }
+        this.handlePasswordInput = this.handlePasswordInput.bind(this);
+        this.handleUsernameInput = this.handleUsernameInput.bind(this);
+    }
+    handleUsernameInput(e){
+        let usernameRegex = /^[a-zA-Z0-9]{5,30}$/;
+        this.setState({validUsername : usernameRegex.test(e.target.value)})
+    }
+    handlePasswordInput(e){
+        let passwordRegex = /^.{7,200}$/;
+        this.setState({validPassword : passwordRegex.test(e.target.value)})
+        this.setState({passwordsMatch : (document.querySelector("#confirmPassword").value === document.querySelector("#loginPassword").value)});
+    }
   render() {
     return (
       <div>
         <div className="AuthenticationForm">
           <h4>Register for free</h4>
-          <input type="text" placeholder="Username" formNoValidate />
-          <input type="password" placeholder="Password" formNoValidate />
+          <input id="loginName" className={this.state.validUsername? "" : "badInput"} onChange = {this.handleUsernameInput} type="text" placeholder="Username" formNoValidate />
+          <input id="loginPassword" className={this.state.validPassword? "" : "badInput"} onChange = {this.handlePasswordInput} type="password" placeholder="Password" formNoValidate />
           <input
+            className={this.state.passwordsMatch? "" : "badInput"}
+            id="confirmPassword"
             type="password"
             placeholder="Confirm Password"
             formNoValidate
