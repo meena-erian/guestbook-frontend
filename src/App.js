0d63da21426b43a737dfe6bd93909f29e43124bc
Meena Erian <meena5.erian@gmail.com>
2020-03-22 05:33:45 +0200

    Revert "render main"
    This reverts commit 0081ed491879294313ffeab6f352daaa5c2faa0c.

--- src/App.js
+++ src/App.js
@@ -2,59 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
-class MainPage extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Welcome {this.props.login}</h1>
-      </div>
-    );
-  }
-}
-
-class LoginPage extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Login to continue</h1>
-      </div>
-    );
-  }
-}
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    if (-1 === document.cookie.indexOf("guestname")) {
-      //Then this visitor isn't logged in
-      this.state = {
-        login: false
-      };
-    } else {
-      this.state = {
-        login: document.cookie.split("guestname=", 2)[1].split(";", 2)[0]
-      };
-    }
-  }
-
-  render() {
-    const login = this.state.login;
-    let page;
-    if (login) {
-      page = <MainPage login = {login}/>;
-    } else {
-      page = <LoginPage />;
-    }
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          {page}
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
