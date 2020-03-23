d9f1ec4b89c158127bdaee79d837143639d9c5e2
Meena Erian <meena5.erian@gmail.com>
2020-03-23 18:59:51 +0200

    Connected the signup form with the backend api and configured it to save the login session cookies

--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -6,10 +6,13 @@ class SignupPage extends React.Component {
         this.state = {
             validUsername : undefined,
             validPassword : undefined,
-            passwordsMatch : undefined
+            passwordsMatch : undefined,
+            tempUsername : ""
         }
         this.handlePasswordInput = this.handlePasswordInput.bind(this);
         this.handleUsernameInput = this.handleUsernameInput.bind(this);
+        this.signupRequestSender = this.signupRequestSender.bind(this);
+        this.signupResponseHandler = this.signupResponseHandler.bind(this);
     }
     handleUsernameInput(e){
         let usernameRegex = /^[a-zA-Z0-9]{5,30}$/;
@@ -20,6 +23,26 @@ class SignupPage extends React.Component {
         this.setState({validPassword : passwordRegex.test(e.target.value)})
         this.setState({passwordsMatch : (document.querySelector("#confirmPassword").value === document.querySelector("#loginPassword").value)});
     }
+    signupRequestSender(){
+      var btn = document.querySelector('#signup-button');
+      btn.classList.add("loading");
+      this.state.tempUsername = document.querySelector("#loginName").value
+      api("POST", "/user/signup", this.signupResponseHandler, { // eslint-disable-line
+        'username' : this.state.tempUsername,
+        'password' : document.querySelector("#loginPassword").value
+      });
+    }
+    signupResponseHandler(e){
+      if(e.target.status == 200){
+        var apiResponse = JSON.parse(e.target.response);
+        document.cookie = "guestname=" + this.state.tempUsername + "; sessionid=" + apiResponse.token + ";";
+      }
+      else if(e.target.status == 451){
+        alert("Username is not available");
+      }
+      var btn = document.querySelector('#signup-button');
+      btn.classList.remove("loading");
+    }
   render() {
     return (
       <div>
@@ -28,13 +51,14 @@ class SignupPage extends React.Component {
           <input id="loginName" className={this.state.validUsername !== false? "" : "badInput"} onChange = {this.handleUsernameInput} type="text" placeholder="Username" formNoValidate />
           <input id="loginPassword" className={this.state.validPassword !== false? "" : "badInput"} onChange = {this.handlePasswordInput} type="password" placeholder="Password" formNoValidate />
           <input
+            onChange={this.handlePasswordInput}
             className={this.state.passwordsMatch !== false ? "" : "badInput"}
             id="confirmPassword"
             type="password"
             placeholder="Confirm Password"
             formNoValidate
           />
-          <input type="button" value="Sign up" disabled={!this.state.validUsername || !this.state.validPassword || !this.state.passwordsMatch} />
+          <input id='signup-button' onClick={this.signupRequestSender} type="button" value="Sign up" disabled={!this.state.validUsername || !this.state.validPassword || !this.state.passwordsMatch} />
         </div>
       </div>
     );
